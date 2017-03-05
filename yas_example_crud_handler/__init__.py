e7bd4f684688b8d4dcc73e94a81900310a4ade8f
Schlueter <bs@schlueter.blue>
2017-03-05 04:17:54 -0500

    pass log function itself

--- yas_example_crud_handler/__init__.py
+++ yas_example_crud_handler/__init__.py
@@ -6,26 +6,26 @@ from yas_example_crud_handler.yaml_file_config import YamlConfiguration
 
 class ExampleCludHandler(YasHandler, YamlConfiguration):
 
-    def __init__(self, regexp_string, logger=print):
+    def __init__(self, regexp_string, log=print):
         super(YasHandler).__init__()
         self.regexp = re.compile(regexp_string)
-        logger.log.debug(f"{self.__class__} initialized and matching {regexp_string}!")
+        log.debug(f"{self.__class__} initialized and matching {regexp_string}!")
 
     def test(self, data):
-        logger.log.debug(f"Testing {data['yas_hash']} against {self.__class__}")
+        log.debug(f"Testing {data['yas_hash']} against {self.__class__}")
         self.current_match = self.regexp.match(data.get('text'))
 
 
 class ExampleCreateHandler(ExampleCludHandler):
 
-    def __init__(self, logger=print):
+    def __init__(self, log=print):
         super().__init__('(?:create)\ ([-\w]+)')
 
     def handle(self, data, reply):
-        logger.log.info(f"Handling {data['yas_hash']} with {self.__class__}")
+        log.info(f"Handling {data['yas_hash']} with {self.__class__}")
         match_groups = self.current_match.groups()
         self.__create(*match_groups, reply=reply)
 
     def __create(self, thing_to_create):
         reply(f'Creating {thing_to_create}')
-        logger.log.info(f"Created {thing_to_create}")
+        log.info(f"Created {thing_to_create}")
