1c5727ce288cd06943b6416fb002b2ef2d09c800
Schlueter <bs@schlueter.blue>
2017-03-26 04:25:47 -0400

    Add ExampleSleepHandler

--- yas_example_handlers/__init__.py
+++ yas_example_handlers/__init__.py
@@ -1,3 +1,5 @@
+import time
+
 from yas import RegexHandler
 from yas_example_handlers.yaml_file_config import YamlConfiguration
 
@@ -14,3 +16,13 @@ class ExampleCreateHandler(RegexHandler):
     def __create(self, thing_to_create, reply=None):
         reply(f'Creating {thing_to_create}')
         self.log('INFO', f"Created {thing_to_create}")
+
+class ExampleSleepHandler(RegexHandler):
+
+    def __init__(self, bot_name, api_call, log=print):
+        super().__init__('(?:sleep)\ ([-\d]+)', bot_name, api_call, log=log)
+
+    def handle(self, data, reply):
+        sleep_seconds = int(self.current_match.groups()[0])
+        time.sleep(sleep_seconds)
+        reply(f'Slept for {sleep_seconds}')
